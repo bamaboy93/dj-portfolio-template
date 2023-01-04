e609a41647334e48490e7488b14870a38f188e15
Alexander Krupka <bamaboy483@gmail.com>
2023-01-04 14:11:35 +0200

    +nav-bar

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+  function menuscroll() {
+    var $navmenu = $(".nav-menu");
+    if ($(window).scrollTop() > 50) {
+      $navmenu.addClass("is-scrolling");
+    } else {
+      $navmenu.removeClass("is-scrolling");
+    }
+  }
+  menuscroll();
+  $(window).on("scroll", function () {
+    menuscroll();
+  });
+});
